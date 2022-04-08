0aebbb4910ad3d4f802ee4e554cc61d2a9fd5e73
Federico Belotti <federico.belotti@outlook.com>
2022-04-08 12:33:33 +0200

    Re-organized namespaces.

--- OpenSea/Client/OpenseaClient.cs
+++ OpenSea/Client/OpenseaClient.cs
@@ -1,8 +1,8 @@
-﻿using FloorTracker.Models;
+﻿using FloorTracker.OpenSea.Models;
 using Newtonsoft.Json;
 using RestSharp;
 
-namespace FloorTracker
+namespace FloorTracker.OpenSea.Client
 {
     /// <summary>
     /// Class that implements OpenSea APIs to obtain collections stats.
@@ -13,7 +13,7 @@ namespace FloorTracker
         /// <summary>
         /// OpenSea API endpoint.
         /// </summary>
-        private string _apiEndpoint = "https://api.opensea.io/api/v1";
+        private readonly string _apiEndpoint = "https://api.opensea.io/api/v1";
         private string _apiKey = String.Empty;
 
         /// <summary>
@@ -28,25 +28,27 @@ namespace FloorTracker
         public OpenseaClient(string apiKey) => _apiKey = apiKey;
 
         /// <summary>
+        /// Gets all given collection stats asynchronously as a JSON object.
+        /// </summary>
+        /// <param name="collectionSlug">The collection slug (ID).</param>
+        /// <returns><see cref="RestResponse"/> containing the JSON object.</returns>
+        private async Task<RestResponse> _GetCollectionStatsAsync(string collectionSlug)
+        {
+            using var client = new RestClient($"{_apiEndpoint}/collection/{collectionSlug}/stats");
+            var request = new RestRequest();
+            request.AddHeader("Accept", "application/json");
+            return await client.ExecuteAsync(request);
+        }
+
+        /// <summary>
         /// Gets all given collection stats asynchronously.
         /// </summary>
         /// <param name="collectionSlug">The collection slug (ID).</param>
-        /// <returns></returns>
-        private async Task<RestResponse> _GetCollectionStatsAsync(string collectionSlug)
-        {
-            using(var client = new RestClient($"{_apiEndpoint}/collection/{collectionSlug}/stats"))
-            {
-                var request = new RestRequest();
-                request.AddHeader("Accept", "application/json");
-                return await client.ExecuteAsync(request);
-            }
-        }
-
+        /// <returns><see cref="CollectionStats"/> object.</returns>
         public async Task<CollectionStats.RootObject> GetCollectionStatsAsync(string collectionSlug)
         {
-            var response = await _GetCollectionStatsAsync(collectionSlug);
-            CollectionStats.RootObject stats = JsonConvert.DeserializeObject<CollectionStats.RootObject>(response.Content);
-            return stats;
+            RestResponse response = await _GetCollectionStatsAsync(collectionSlug);
+            return JsonConvert.DeserializeObject<CollectionStats.RootObject>(response.Content);
         }
     }
 }
---
+++ OpenSea/Models/CollectionStats.cs
@@ -0,0 +1,35 @@
+﻿namespace FloorTracker.OpenSea.Models
+{
+    public class CollectionStats
+    {
+        public class RootObject
+        {
+            public Stats Stats { get; set; }
+        }
+
+        public class Stats
+        {
+            public float One_day_volume { get; set; }
+            public float One_day_change { get; set; }
+            public float One_day_sales { get; set; }
+            public float One_day_average_price { get; set; }
+            public float Seven_day_volume { get; set; }
+            public float Seven_day_change { get; set; }
+            public float Seven_day_sales { get; set; }
+            public float Seven_day_average_price { get; set; }
+            public float Thirty_day_volume { get; set; }
+            public float Thirty_day_change { get; set; }
+            public float Thirty_day_sales { get; set; }
+            public float Thirty_day_average_price { get; set; }
+            public float Total_volume { get; set; }
+            public float Total_sales { get; set; }
+            public float Total_supply { get; set; }
+            public float Count { get; set; }
+            public int Num_owners { get; set; }
+            public float Average_price { get; set; }
+            public int Num_reports { get; set; }
+            public float Market_cap { get; set; }
+            public float Floor_price { get; set; }
+        }
+    }
+}
